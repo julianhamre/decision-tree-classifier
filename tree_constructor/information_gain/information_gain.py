6780e64d159a83bcd20ceb42c27e8d18f6ccf872
Julian Hamre <julian.hamre5@gmail.com>
2026-09-13 11:36:42 +0200

    Improve information gain code

--- tree_constructor/information_gain/information_gain.py
+++ tree_constructor/information_gain/information_gain.py
@@ -1,44 +1,50 @@
 import numpy as np
 
 from decision_tree.feature_thresholds import numerical_threshold
-from tree_constructor.information_gain.categorical_purity_index import CategoricalPurityIndex
-from tree_constructor.information_gain.numerical_purity_index import NumericalPurityIndex
+from tree_constructor.information_gain.purity_index import PurityIndex
 
 class InformationGain:
 
     def __init__(self, data, purity_index="entropy"):
         self.__data = data
-        self.__numerical_purity = NumericalPurityIndex(index_type=purity_index)
-        self.__categorical_purity = CategoricalPurityIndex(index_type=purity_index)
+        self.__purity = PurityIndex(index_type=purity_index)
 
-    def __categorical_conditional_purity(self, feature_values, labels):
+    def __probability_of_numerical_feature_value(self, feature_value, feature_values):
+        threshold = numerical_threshold(feature_values)
+        binarized_on_threshold = feature_values > threshold 
+        if feature_value <= threshold:
+            return float( (len(binarized_on_threshold) - np.count_nonzero(binarized_on_threshold)) / len(binarized_on_threshold) )
+        else:
+            return float( np.count_nonzero(binarized_on_threshold) / len(binarized_on_threshold) )
+
+    def __purity_given_categorical_feature(self, feature_values, labels):
         purity = 0.0
         unique_feature_values = np.unique(feature_values)
         for i in range(len(unique_feature_values)):
             feature_value = unique_feature_values[i]
-            probability_of_feature_value = self.__categorical_purity.probability_of_value(feature_value, feature_values)
+            probability_of_feature_value = self.__purity.probability_of_value(feature_value, feature_values)
             labels_given_feature_value = labels[feature_values == feature_value]
-            purity += probability_of_feature_value * self.__categorical_purity.calculate(labels_given_feature_value)
+            purity += probability_of_feature_value * self.__purity.calculate(labels_given_feature_value)
         return float(purity)
 
-    def __numerical_conditional_purity(self, feature_values, labels):
+    def __purity_given_numerical_feature(self, feature_values, labels):
         purity = 0.0
         threshold = numerical_threshold(feature_values)
         exhaustive_feature_values = [threshold - 1, threshold + 1]
         for feature_value in exhaustive_feature_values:
-            probability_of_value = self.__numerical_purity.probability_of_value(feature_value, feature_values) 
+            probability_of_value = self.__probability_of_numerical_feature_value(feature_value, feature_values) 
             if feature_value <= threshold:
                 labels_given_feature_value = labels[feature_values <= threshold]
             else:
                 labels_given_feature_value = labels[feature_values > threshold]
-            purity += probability_of_value * self.__categorical_purity.calculate(labels_given_feature_value)
+            purity += probability_of_value * self.__purity.calculate(labels_given_feature_value)
         return float(purity)
 
-    def __categorical_information_gain(self, feature_values, labels):
-        return self.__categorical_purity.calculate(labels) - self.__categorical_conditional_purity(feature_values, labels)
+    def __information_gain_given_categorical_feature(self, feature_values, labels):
+        return self.__purity.calculate(labels) - self.__purity_given_categorical_feature(feature_values, labels)
 
-    def __numerical_information_gain(self, feature_values, labels):
-        return self.__categorical_purity.calculate(labels) - self.__numerical_conditional_purity(feature_values, labels)
+    def __information_gain_given_numerical_feature(self, feature_values, labels):
+        return self.__purity.calculate(labels) - self.__purity_given_numerical_feature(feature_values, labels)
 
     def feature_with_highest_information_gain(self):
         best_col = 0
@@ -47,9 +53,9 @@ class InformationGain:
         for feature_col in range(self.__data.num_features()):
             feature_values = self.__data.feature_values(feature_col)
             if self.__data.is_numerical_feature(feature_col):
-                information_gain = self.__numerical_information_gain(feature_values, labels)
+                information_gain = self.__information_gain_given_numerical_feature(feature_values, labels)
             else:
-                information_gain = self.__categorical_information_gain(feature_values, labels)
+                information_gain = self.__information_gain_given_categorical_feature(feature_values, labels)
             if information_gain > highest_information_gain:
                 best_col = feature_col
                 highest_information_gain = information_gain
--- tree_constructor/information_gain/purity_index.py
+++ tree_constructor/information_gain/purity_index.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
 import numpy as np
 
-class PurityIndex(ABC):
+class PurityIndex():
+    """A class for calculating the purity of a categorical data column"""
 
     def __init__(self, index_type="entropy"):
         self.__set_purity_index(index_type)
@@ -18,16 +18,19 @@ class PurityIndex(ABC):
     def __gini_term(self, probability):
         return probability * (1 - probability)
 
-    @abstractmethod
-    def _calculate(self, values, index_term) -> float:
-        pass
+    def __calculate(self, values, index_term):
+        purity = 0.0 
+        for value in np.unique(values):
+            probability = self.probability_of_value(value, values)
+            purity += index_term(probability)
+        return float(purity)
 
     def __entropy(self, values):
-        return self._calculate(values, self.__entropy_term)
+        return self.__calculate(values, self.__entropy_term)
 
     def __gini(self, values):
-        return self._calculate(values, self.__gini_term) 
+        return self.__calculate(values, self.__gini_term) 
 
-    @abstractmethod
-    def probability_of_value(self, value, values) -> float:
-        pass
+    def probability_of_value(self, value, values):
+        """Calculates the probability of seeing the given value in a column of categorical values"""
+        return float( np.count_nonzero(values == value) / len(values) )
